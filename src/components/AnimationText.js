a824af6621e790d2165070a3bd80da976dde8f6b
Gyeonghun Park <gp112795@gmail.com>
2020-12-18 12:09:26 -0500

    Improve Animation

--- src/components/AnimationText.js
+++ src/components/AnimationText.js
@@ -1,10 +1,18 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
-const AnimationText = ({ text }) => {
+const AnimationText = ({ text, milliseconds }) => {
   const textRef = useRef();
+
+  useEffect(() => {
+    setTimeout(
+      () => textRef.current.classList.add("animate__bounceIn"),
+      milliseconds
+    );
+  });
 
   const addAnimation = () => {
     textRef.current.classList.remove("animate__bounceIn");
+    textRef.current.classList.remove("opacity-0");
     textRef.current.classList.add("animate__rubberBand");
     textRef.current.onanimationend = () => {
       textRef.current.classList.remove("animate__rubberBand");
@@ -14,7 +22,7 @@ const AnimationText = ({ text }) => {
   return (
     <div
       ref={textRef}
-      className={`p-5 text-white animate__animated animate__bounceIn`}
+      className={`p-3 text-5xl text-white animate__animated opacity-0 hover:text-prime`}
       onMouseEnter={addAnimation}
     >
       {text}
--- src/pages/Home/TextCotainer.js
+++ src/pages/Home/TextCotainer.js
@@ -5,11 +5,17 @@ const TextCotainer = () => {
   return (
     <div className="relative p-10 flex">
       <Tag top="0" left="10" text="h1" />
-      <AnimationText text="H" />
-      <AnimationText text="e" />
-      <AnimationText text="l" />
-      <AnimationText text="l" />
-      <AnimationText text="o" />
+      <AnimationText text="H" milliseconds={1000} />
+      <AnimationText text="e" milliseconds={1100} />
+      <AnimationText text="l" milliseconds={1200} />
+      <AnimationText text="l" milliseconds={1300} />
+      <AnimationText text="o" milliseconds={1400} />
+      <AnimationText text="H" milliseconds={1500} />
+      <AnimationText text="e" milliseconds={1600} />
+      <AnimationText text="l" milliseconds={1700} />
+      <AnimationText text="l" milliseconds={1800} />
+      <AnimationText text="o" milliseconds={1900} />
+
       <Tag bottom="0" left="10" text="/h1" />
     </div>
   );
