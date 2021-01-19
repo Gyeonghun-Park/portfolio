831058e2c11732081459986e1273065eaf05f151
Gyeonghun Park <gp112795@gmail.com>
2021-01-18 23:26:24 -0500

    Netlify Part 2

--- src/pages/Contact/Right/Map.js
+++ src/pages/Contact/Right/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 import GoogleMapReact from "google-map-react";
-import markerIcon from "../../../img/Marker.svg";
+import markerIcon from "../../../img/marker.svg";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
 
