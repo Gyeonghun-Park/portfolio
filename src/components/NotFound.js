86409620db709ad0096d3974c492c93c2f19b998
Gyeonghun Park <gp112795@gmail.com>
2021-02-06 00:00:06 -0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,8 @@
 const NotFound = () => {
   return (
-    <div className="text-white h-full w-full flex align-center">Not found</div>
+    <div className="text-white h-full w-full flex align-center justify-center">
+      Not found
+    </div>
   );
 };
 
