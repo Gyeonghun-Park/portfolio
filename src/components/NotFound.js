541aa7838e7177a38b49ef0d111e01138f8806dc
Gyeonghun Park <gp112795@gmail.com>
2021-02-05 23:59:43 -0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,7 @@
 const NotFound = () => {
-  return <div className="text-white h-full w-full">Not found</div>;
+  return (
+    <div className="text-white h-full w-full flex align-center">Not found</div>
+  );
 };
 
 export default NotFound;
